f1bfe13e4008178997d5475e4356430374e28beb
Nabil Qosthalany <nabilzqosthalany.com>
2024-04-28 17:31:19 +0700

    upwork project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,html}", // Tambahkan jalur ke file JavaScript jika ada
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
